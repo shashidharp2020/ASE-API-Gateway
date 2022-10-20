1eb1f4845c7b57d93459e2245d5c24dc2bd3fd79
shashidharp2020 <70366476+shashidharp2020@users.noreply.github.com>
2022-10-20 11:52:06 +0530

    Changes to the documentation and service installer

--- service.js
+++ service.js
@@ -5,13 +5,13 @@ const appFilePath = path.resolve(path.join(__dirname, 'server.js'));
 var cmdprogram = require('commander');
 const serviceName = "ASEAPIGateway";
 
-var issueGatewayService = new service({
+var aseAPIService = new service({
     name: 'ASE API Gateway',
     description: 'ASE API Gateway',
     script: appFilePath
 });
 
-issueGatewayService._directory =  path.resolve();
+aseAPIService._directory =  path.resolve();
 
 cmdprogram
     .option('--install', 'Install the service')
@@ -24,14 +24,14 @@ if(cmdprogram.uninstall) {
         return console.log('The %s service is not installed.', serviceName);
     
     console.log('Uninstalling the %s service...', serviceName);
-    issueGatewayService.uninstall();    
+    aseAPIService.uninstall();    
 }
 else if(cmdprogram.install) {
     if (verifyServiceInstalled())
         return console.log('The %s service is already installed', serviceName);
 
     console.log('Installing the service %s ', serviceName);
-    issueGatewayService.install();
+    aseAPIService.install();
 }
 else 
     return cmdprogram.help();
@@ -57,18 +57,18 @@ function verifyServiceInstalled() {
 
 // Listen for the "install" event, which indicates the
 // process is available as a service.
-issueGatewayService.on('install', function () {
+aseAPIService.on('install', function () {
     if(verifyServiceInstalled())
         return console.log('The service %s is installed.', serviceName);
 });
 
 // Listen for the "uninstall" event, to uninstall service
-issueGatewayService.on('uninstall', function () {
+aseAPIService.on('uninstall', function () {
     if(!verifyServiceInstalled()) 
         return console.log('The service %s is uninstalled.', serviceName);
 });
 
 // Listen for the "uninstall" event, to uninstall service
-issueGatewayService.on('error', function () {
+aseAPIService.on('error', function () {
     return console.log('Failed to install the service - ' + error);
 });
